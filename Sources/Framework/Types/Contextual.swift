c1de75c551edcc3d5457f44f09cdd7a768fc3cd9
William Luke <williamluke@me.com>
2024-05-04 02:11:56 -0500

    remove all assumed states

--- Sources/Framework/Types/Contextual.swift
+++ Sources/Framework/Types/Contextual.swift
@@ -17,10 +17,12 @@ public extension ContextModule {
   try await context.callAsFunction()
  }
 
- static func callContext(
-  with state: ModuleContext.State
- ) async throws {
+ static func callContext(with state: ModuleContext.State) async throws {
   try await context.callAsFunction(with: state)
+ }
+
+ static func cancelContext() async {
+  await context.cancel()
  }
 
  static func cancelContext(with state: ModuleContext.State) async {
@@ -31,51 +33,89 @@ public extension ContextModule {
   try await context.update()
  }
 
+ static func updateContext(with state: ModuleContext.State) async throws {
+  try await context.update(with: state)
+ }
+
  static func waitForContext() async throws {
   try await context.wait()
  }
 }
 
 public extension ContextModule {
- func callContext(
-  with state: ModuleContext.State = .active
- ) async throws {
+ func callContext() async throws {
+  try await Self.callContext()
+ }
+
+ func callContext(with state: ModuleContext.State) async throws {
   try await Self.callContext(with: state)
  }
- 
- func cancelContext(with state: ModuleContext.State = .idle) async {
+
+ func cancelContext() async {
+  await Self.cancelContext()
+ }
+
+ func cancelContext(with state: ModuleContext.State) async {
   await Self.cancelContext(with: state)
  }
- 
+
  func updateContext() async throws {
   try await Self.updateContext()
  }
- 
+
+ func updateContext(with state: ModuleContext.State) async throws {
+  try await Self.updateContext()
+ }
+
  func waitForContext() async throws {
   try await Self.waitForContext()
  }
- 
- func callContext(with state: ModuleContext.State = .active) {
+
+ func callContext() {
+  Task {
+   try await Self.callContext()
+  }
+ }
+
+ func callContext(with state: ModuleContext.State) {
   Task {
    try await Self.callContext(with: state)
   }
  }
- 
- func cancelContext(with state: ModuleContext.State = .idle) {
+
+ func cancelContext() {
+  Task {
+   await Self.cancelContext()
+  }
+ }
+
+ func cancelContext(with state: ModuleContext.State) {
   Task {
    await Self.cancelContext(with: state)
   }
  }
- 
+
  func updateContext() {
   Task {
    try await Self.updateContext()
   }
  }
- 
+
+ func updateContext(with state: ModuleContext.State) {
+  Task {
+   try await Self.updateContext(with: state)
+  }
+ }
+
+ @discardableResult
+ mutating func call<Result>(action: (inout Self) -> Result) -> Result {
+  defer { self.callContext() }
+  return action(&self)
+ }
+
  @discardableResult
  mutating func call<Result>(
-  with state: ModuleContext.State = .active, action: (inout Self) -> Result
+  with state: ModuleContext.State, action: (inout Self) -> Result
  ) -> Result {
   defer { self.callContext(with: state) }
   return action(&self)
